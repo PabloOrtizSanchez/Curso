742864b324f9b137b08feb02bf8d649dc928c4ab
pabloortizsanchez12@gmail.com <pabloortizsanchez12@gmail.com>
2022-11-18 18:43:35 +0000

    creados los modelos en el staging (solo el archivo xd) y probando jinja

--- models/marts/core/dim_fecha.sql
+++ models/marts/core/dim_fecha.sql
@@ -2,7 +2,7 @@ with budget as ()
 {{ dbt_utils.date_spine(
     datepart="day",
     start_date="cast('2019-01-01' as date)",
-    end_date="cast('2020-01-01' as date)"
+    end_date="cast('2022-11-18' as date)"
    )
 }}
 )
