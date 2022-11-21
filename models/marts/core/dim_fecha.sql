471b0c00cedb4906c9a155cce69586ef42700f2e
pabloortizsanchez12@gmail.com <pabloortizsanchez12@gmail.com>
2022-11-21 19:01:46 +0000

    clase dia 21, model de budget se cambia la columna _row por una id con la funcion md5

--- models/marts/core/dim_fecha.sql
+++ models/marts/core/dim_fecha.sql
@@ -1,8 +1,23 @@
-with budget as ()
-{{ dbt_utils.date_spine(
-    datepart="day",
-    start_date="cast('2019-01-01' as date)",
-    end_date="cast('2022-11-18' as date)"
-   )
+{{
+
+  config(
+
+    materialized='table'
+
+  )
+
 }}
-)
+
+
+
+{{ dbt_utils.date_spine(
+
+    datepart="day",
+
+    start_date="cast('2021-01-01' as date)",
+
+    end_date="cast('2023-01-01' as date)"
+
+   )
+
+}}
---
+++ models/staging/google_sheets/stg_google_sheets_budget.sql
@@ -0,0 +1,11 @@
+{{ config(materialized="table") }}
+
+with budget as (select * from {{ source("google_sheets", "budget") }})
+
+select md5(_row) as budget_id
+, quantity
+, month
+, product_id
+, _fivetran_synced
+
+from budget
