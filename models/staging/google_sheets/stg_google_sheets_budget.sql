1e626e0116d94bbff088431d1d3f24b205356065
pabloortizsanchez12@gmail.com <pabloortizsanchez12@gmail.com>
2022-11-22 13:03:07 +0000

    Modelo budget, address, events, promos, order_items y orders en stg

--- models/staging/google_sheets/stg_google_sheets_budget.sql
+++ models/staging/google_sheets/stg_google_sheets_budget.sql
@@ -1,11 +1,7 @@
-{{ config(materialized="table") }}
+{{ config(materialized="view") }}
 
 with budget as (select * from {{ source("google_sheets", "budget") }})
 
-select md5(_row) as budget_id
-, quantity
-, month
-, product_id
-, _fivetran_synced
+select md5(_row) as budget_id, quantity, month, product_id, _fivetran_synced
 
 from budget
--- models/staging/sql_Server_dbo/stg_sql_server_dbo_events.sql
+++ models/staging/sql_Server_dbo/stg_sql_server_dbo_events.sql
@@ -1,20 +1,22 @@
+{{ config(materialized="view") }}
+
 with
     src_events as (select * from {{ source("sql_server_dbo", "events") }}),
 
-    renamed_casted as (
+    events as (
         select
             event_id,
-            page_url,
-            event_type,
             user_id,
             product_id,
             session_id,
-            created_at,
             order_id,
+            page_url,
+            event_type,
+            created_at,
             _fivetran_deleted,
             _fivetran_synced
         from src_events
     )
 
 select *
-from renamed_casted
+from events
