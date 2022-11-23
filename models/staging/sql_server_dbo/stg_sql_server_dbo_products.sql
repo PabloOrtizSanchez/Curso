47da991145d1803662b8e69762b11d78baf511cb
pabloortizsanchez12@gmail.com <pabloortizsanchez12@gmail.com>
2022-11-23 13:05:46 +0000

    Algun pequeño cambio en los modelos y un intento de cambiar el nombre de la columna price de products sin exito

--- models/staging/sql_server_dbo/stg_sql_server_dbo_products.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo_products.sql
@@ -1,12 +1,14 @@
-{{ config(materialized="view") }}
+{{ config(materialized="table") }}
 
 with products as (select * from {{ source("sql_server_dbo", "products") }})
 
+alter table products rename column price to price_($)  -- como se hace eso :(
 
 select
 
       md5(product_id) as product_id
-    , inventory, price
+    , inventory
+    , price_($)
     , name
     , _fivetran_deleted
     , _fivetran_synced
