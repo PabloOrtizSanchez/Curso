faf4e3375c4c529dd8b181d1dd9d2436b684b127
pabloortizsanchez12@gmail.com <pabloortizsanchez12@gmail.com>
2022-12-05 13:57:13 +0000

    modelos en la gold acabados, aunque en el futuro tendre que cambiar cosas

--- models/marts/core/fact_order_details.sql
+++ models/marts/core/fact_order_details.sql
@@ -13,28 +13,20 @@ stg_sql_server_dbo_products as (select * from {{ ref('stg_sql_server_dbo_product
 fact_order_details as (
   
   select
-   a.order_details_id
- , a.order_details_NK_id
- , c.user_id
- , d.product_id
- , a.order_id
- , a.quantity
- , a.quantity * d.prize_USD
- , b.shipping_service
- , b.created_at_id
- , a._fivetran_deleted
- , a._fivetran_synced
+   order_details_id
+ , order_details_NK_id
+ , user_id
+ , product_id
+ , order_id
+ , quantity
+ , prize_USD
+ , quantity * prize_USD
+ , shipping_service
+ , created_at_id
+ , _fivetran_deleted
+ , _fivetran_synced
 
-from stg_sql_server_dbo_orders_items as a
-join
-stg_sql_server_dbo_orders as b
-on a.order_id = b.order_tracking_NK_id
-join
-stg_sql_server_dbo_users as c
-on b.user_id = c.user_id
-join
-stg_sql_server_dbo_products as d
-on b.product_id = d.product_id
+from stg_sql_server_dbo_orders_items
 )
 
 select * from fact_order_details
--- models/staging/sql_server_dbo/stg_sql_server_dbo_orders_items.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo_orders_items.sql
@@ -6,6 +6,8 @@ base_sql_server_dbo_orders as (select * from {{ ref('base_sql_server_dbo_orders'
 ,
 base_sql_server_dbo_products as (select * from {{ ref('base_sql_server_dbo_products') }})
 ,
+base_sql_server_dbo_users as (select * from {{ ref('base_sql_server_dbo_users') }})
+,
 
 
 stg_orders_items as (
@@ -13,9 +15,13 @@ stg_orders_items as (
   select
    order_item_id as order_details_id
  , order_items_NK_id as order_details_NK_id
- , a.order_id
+ , b.order_id
+ , d.user_id
  , c.product_id
  , a.quantity
+ , c.prize_USD
+ , b.shipping_service
+ , b.created_at_id
  , a._fivetran_deleted
  , a._fivetran_synced
 
@@ -26,6 +32,9 @@ on a.order_id = b.order_NK_id
 join 
 base_sql_server_dbo_products as c
 on a.product_id = c.product_NK_id
+join
+base_sql_server_dbo_users as d
+on b.user_id = d.user_NK_id
 )
 
 select * from stg_orders_items
